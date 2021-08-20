57df27ac5f55e803cb4c9fd0b9bcaaf04be0ebca
Joaquin Loiza <loiza.joaquin@gmail.com>
2021-08-20 11:34:55 -0300

    agrega correcciones de nombres de variables y refactor de codigo

--- app/src/main/java/proemprender/activities/ProductsActivity.java
+++ app/src/main/java/proemprender/activities/ProductsActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -15,7 +14,7 @@ import proemprender.model.DbAdapter;
 
 public class ProductsActivity extends AppCompatActivity {
     ListView products;
-    ArrayList<String> listaDatos;
+    ArrayList<String> dataList;
     DbAdapter helper;
 
     @Override
@@ -26,33 +25,29 @@ public class ProductsActivity extends AppCompatActivity {
         products= findViewById(R.id.dinamic_products);
         helper = new DbAdapter(this);
         loadProducts();
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listaDatos);
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataList);
         products.setAdapter(adapter);
     }
 
     private void newProductActivity(){
-        Button btn_products = (Button) findViewById(R.id.btn_newProduct);
-        btn_products.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ProductsActivity.this, NewProductActivity.class));
-            }
-        });
+        Button btn_products = findViewById(R.id.btn_newProduct);
+        btn_products.setOnClickListener(view ->
+                startActivity(new Intent(ProductsActivity.this, NewProductActivity.class)));
     }
 
     private void loadProducts() {
 
-        listaDatos = new ArrayList<>();
-        ArrayList<Product> listProduct = new ArrayList<>();
+        dataList = new ArrayList<>();
+        ArrayList<Product> productList = new ArrayList<>();
         Cursor cursor = helper.getProducts();
 
         while (cursor.moveToNext()){
             Product product = new Product(cursor.getInt(0), cursor.getString(1));
-            listProduct.add(product);
+            productList.add(product);
         }
 
-        for (Product p : listProduct) {
-            listaDatos.add(p.getName());
+        for (Product p : productList) {
+            dataList.add(p.getName());
         }
     }
 }
--- app/src/main/java/proemprender/model/DbAdapter.java
+++ app/src/main/java/proemprender/model/DbAdapter.java
@@ -13,18 +13,16 @@ public class DbAdapter {
         dbHelper = new DbHelper(context);
     }
 
-    public long insertProduct(String name) {
+    public void insertProduct(String name) {
         SQLiteDatabase dbb = dbHelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(DbHelper.NAME, name);
-        long id = dbb.insert(DbHelper.TABLE_NAME, null , contentValues);
+        dbb.insert(DbHelper.TABLE_NAME, null , contentValues);
         dbb.close();
-        return id;
     }
 
     public Cursor getProduct( Integer id ) {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-        String[] columns = {DbHelper.UID, DbHelper.NAME};
         Cursor cursor = db.rawQuery("SELECT * FROM "+ DbHelper.TABLE_NAME + " WHERE "+ DbHelper.UID +" = "+ id + ";",null);
         db.close();
         return cursor;
@@ -34,19 +32,21 @@ public class DbAdapter {
     public Cursor getProducts() {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         String[] columns = {DbHelper.UID, DbHelper.NAME};
-        Cursor cursor =  db.query(DbHelper.TABLE_NAME, columns, null, null, null, null, null);
-        return cursor;
+        return db.query(DbHelper.TABLE_NAME, columns, null, null, null, null, null);
     }
 
 
     //-----------------------------------------------------------------------------------------------------------------
     static class DbHelper extends SQLiteOpenHelper {
-        private static final String DATABASE_NAME = "DB_ProEmprender";    // Database Name
+
+        //----- Table products
         private static final String TABLE_NAME = "products";   // Table Name
-        private static final int DATABASE_Version = 1;    // Database Version
         private static final String UID="id";     // Column I (Primary Key)
         private static final String NAME = "name";    //Column II
         private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+" ("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+NAME+" VARCHAR(255));";
+
+        private static final String DATABASE_NAME = "DB_ProEmprender";    // Database Name
+        private static final int DATABASE_Version = 1;    // Database Version
         private static final String DROP_TABLE ="DROP TABLE IF EXISTS "+TABLE_NAME;
 
         public DbHelper(Context context) {
