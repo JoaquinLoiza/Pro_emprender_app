48c3a1dfadf2f7a7eb4a1610e8fef4450331f226
Joaquin Loiza <loiza.joaquin@gmail.com>
2021-08-26 22:10:32 -0300

    Agrega validacion de campos vacios

--- app/src/main/java/proemprender/activities/ProductsActivity.java
+++ app/src/main/java/proemprender/activities/ProductsActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.io.Serializable;
@@ -29,17 +31,19 @@ public class ProductsActivity extends AppCompatActivity implements NewProductDia
         productsListView = findViewById(R.id.dinamic_products);
         helper = new DbAdapter(this);
         productList = new ArrayList<>();
+        refreshProducts();
         onClickProduct();
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+
+    protected void refreshProducts() {
         loadProducts();
         adapter = new ListAdapterProducts(this, R.layout.card_product, productList);
         productsListView.setAdapter(adapter);
     }
 
+    //Escucha el boton para el dialogo de agregar nuevo producto
     private void newProductActivity(){
         FloatingActionButton btn_products = findViewById(R.id.btn_newProduct);
         btn_products.setOnClickListener(view -> {
@@ -47,6 +51,7 @@ public class ProductsActivity extends AppCompatActivity implements NewProductDia
         });
     }
 
+    //Trae todos los productos de la base de datos y crea los objetos "Product" de cada uno
     private void loadProducts() {
         adapter = null;
         productList.clear();
@@ -58,6 +63,7 @@ public class ProductsActivity extends AppCompatActivity implements NewProductDia
         }
     }
 
+    //Escucha los clicks de cada item producto, y envia el objeto clickeado hacia otra actividad
     private void onClickProduct(){
         productsListView.setOnItemClickListener((parent, view, position, id) -> {
         Product p = productList.get(position);
@@ -69,15 +75,25 @@ public class ProductsActivity extends AppCompatActivity implements NewProductDia
         });
     }
 
+    //Muestra el dialogo con los inputs para agregar un producto
     public void openDialog() {
         NewProductDialog Dialog = new NewProductDialog();
         Dialog.show(getSupportFragmentManager(), "dialog");
     }
 
+    //Inserta el producto en la base de datos
     @Override
     public void setInputValues(String name, String price) {
-        int priceInt = Integer.parseInt(price);
-        helper.insertProduct(name, priceInt);
-        onResume();
+        if (name.isEmpty()) {
+            Toast.makeText(this, "El campo nombre está vacio", Toast.LENGTH_LONG).show();
+        }
+        else if (price.isEmpty()){
+            Toast.makeText(this, "El campo precio está vacio", Toast.LENGTH_LONG).show();
+        }
+        else {
+            int priceInt = Integer.parseInt(price);
+            helper.insertProduct(name, priceInt);
+            refreshProducts();
+        }
     }
 }
